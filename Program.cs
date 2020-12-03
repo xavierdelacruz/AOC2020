9100f986284ac60f6f3d3f27ac6761704cf7ccf1
xavierdelacruz <xdelacruz89@gmail.com>
2020-12-03 00:03:19 -0800

    Added day 3 for AOC

--- Program.cs
+++ Program.cs
@@ -8,11 +8,21 @@ namespace AOC2020
     {
         static void Main(string[] args)
         {
-            var day1 = new Day1();
+            string[] initialInput = File.ReadAllLines(@"C:\Users\xdela\Documents\Git\AOC2020\Input Day3.txt");
+            char[][] processedInput = new char[initialInput.Length][];
+            
+            for (int i = 0; i < initialInput.Length; i++) {
+                var charArr = initialInput[i].ToCharArray();
+                processedInput[i] = charArr;
+            }
 
-            var input = File.ReadAllLines(@"C:\Users\xdela\Documents\Git\AOC2020\Input Day2.txt");
-            var day2 = new Day2();
-            Console.WriteLine(day2.ValidatePositions(day2Input));
+            var day3 = new Day3();
+            var res1 = day3.TraverseToBottom(processedInput, (1, 1));
+            var res2 = day3.TraverseToBottom(processedInput, (1, 3));
+            var res3 = day3.TraverseToBottom(processedInput, (1, 5));
+            var res4 = day3.TraverseToBottom(processedInput, (1, 7));
+            var res5 = day3.TraverseToBottom(processedInput, (2, 1));
+            Console.WriteLine(res1 * res2 * res3 * res4 * res5);
         }
     }
 }
